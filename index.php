30b8e7b0e79c9b52a8593203afba5f57206fea96
Sean <spward@coastal.edu>
2019-05-04 17:52:10 -0400

    Fixed Index styling

--- index.php
+++ index.php
@@ -1,229 +1,215 @@
-<?php require_once "header.php";?>
+<?php require_once "header.php"; ?>
 
 <!-- Hero Unit
 		================================================== -->
-		<div class="hero-unit">
-			<div class="container hero-unit__container">
-				<div class="hero-unit__content hero-unit__content--left-center">
-					<h5 class="hero-unit__subtitle">That Cougar Life</h5>
-					<h1 class="hero-unit__title">Confidence <span class="text-primary">Cougars</span></h1>
-					<div class="hero-unit__desc"><p>Welcome to Confidence Hoops, Home of the Confidence Cougars!</p>
+<div class="hero-unit">
+	<div class="container hero-unit__container">
+		<div class="hero-unit__content hero-unit__content--left-center">
+			<h5 class="hero-unit__subtitle">That Cougar Life</h5>
+			<h1 class="hero-unit__title">Confidence <span class="text-primary">Cougars</span></h1>
+			<div class="hero-unit__desc">
+				<p>Welcome to Confidence Hoops, Home of the Confidence Cougars!</p>
 
-<p>Stationed out of Horry County, Confidence Hoops is a competitive travel basketball program that stresses fundamental skill mastery, building of the basketball IQ, teamwork, and fellowship/sportsmanship through participation in ASGR, NTBA, and USS tournaments.  Confidence Hoops, now in its 6th year, remains a summer and off-season training program for aspiring collegiate athletes.</p>
+				<p>Stationed out of Horry County, Confidence Hoops is a competitive travel basketball program that stresses fundamental skill mastery, building of the basketball IQ, teamwork, and fellowship/sportsmanship through participation in ASGR, NTBA, and USS tournaments. Confidence Hoops, now in its 6th year, remains a summer and off-season training program for aspiring collegiate athletes.</p>
 
-<p>Elite training, player development, and community relationships are the ideals that define our program.  With over hundreds of years of coaching experience and multiple levels of experienced play, we are CONFIDENT that Confidence Hoops provides the most complete developmental experience for players of all ages.</p>
+				<p>Elite training, player development, and community relationships are the ideals that define our program. With over hundreds of years of coaching experience and multiple levels of experienced play, we are CONFIDENT that Confidence Hoops provides the most complete developmental experience for players of all ages.</p>
 
-</div>
-					<a href="about-us.php" class="btn btn-inverse btn-sm btn-outline btn-icon-right btn-condensed hero-unit__btn">Read More <i class="fa fa-plus text-primary"></i></a>
-				</div>
-		
-				<!-- <figure class="hero-unit__img">
+			</div>
+			<a href="about-us.php" class="btn btn-inverse btn-sm btn-outline btn-icon-right btn-condensed hero-unit__btn">Read More <i class="fa fa-plus text-primary"></i></a>
+		</div>
+
+		<!-- <figure class="hero-unit__img">
 					<img src="assets/images/samples/header_player.png" alt="Hero Unit Image">
 				</figure> -->
-			</div>
-		</div>
+	</div>
+</div>
 
-		<!-- Header Featured News
-		================================================== -->
-		<div class="posts posts--carousel-featured featured-carousel">
-		
-			<div class="posts__item posts__item--category-1">
-				<a href="#" class="posts__link-wrapper">
-					<figure class="posts__thumb">
-						<img src="assets/images/samples/featured-carousel-2.jpg" alt="">
-					</figure>
-					<div class="posts__inner">
-						<div class="posts__cat">
-							<span class="label posts__cat-label">The Team</span>
-						</div>
-						<h3 class="posts__title">Featured STory 1</h3>
-						<time datetime="2019-04-07" class="posts__date">April 7, 2019</time>
-						<ul class="post__meta meta">
-							<li class="meta__item meta__item--views">2369</li>
-							<li class="meta__item meta__item--likes"><i class="meta-like icon-heart"></i> 530</li>
-							<li class="meta__item meta__item--comments">18</li>
-						</ul>
-					</div>
-				</a>
-			</div>
-			<div class="posts__item posts__item--category-1">
-				<a href="#" class="posts__link-wrapper">
-					<figure class="posts__thumb">
-						<img src="assets/images/samples/featured-carousel-3.jpg" alt="">
-					</figure>
-					<div class="posts__inner">
-						<div class="posts__cat">
-							<span class="label posts__cat-label">The Team</span>
-						</div>
-						<h3 class="posts__title">Checkout the new ride of our best player of the season</h3>
-						<time datetime="2017-08-23" class="posts__date">August 23rd, 2018</time>
-						<ul class="post__meta meta">
-							<li class="meta__item meta__item--views">2369</li>
-							<li class="meta__item meta__item--likes"><i class="meta-like icon-heart"></i> 530</li>
-							<li class="meta__item meta__item--comments">18</li>
-						</ul>
-					</div>
-				</a>
-			</div>
-			<div class="posts__item posts__item--category-1">
-				<a href="#" class="posts__link-wrapper">
-					<figure class="posts__thumb">
-						<img src="assets/images/samples/featured-carousel-1.jpg" alt="">
-					</figure>
-					<div class="posts__inner">
-						<div class="posts__cat">
-							<span class="label posts__cat-label">The Team</span>
-						</div>
-						<h3 class="posts__title">All the players are taking a team trip this summer</h3>
-						<time datetime="2017-08-23" class="posts__date">August 23rd, 2018</time>
-						<ul class="post__meta meta">
-							<li class="meta__item meta__item--views">2369</li>
-							<li class="meta__item meta__item--likes"><i class="meta-like icon-heart"></i> 530</li>
-							<li class="meta__item meta__item--comments">18</li>
-						</ul>
-					</div>
-				</a>
-			</div>
-			<div class="posts__item posts__item--category-1">
-				<a href="#" class="posts__link-wrapper">
-					<figure class="posts__thumb">
-						<img src="assets/images/samples/featured-carousel-2.jpg" alt="">
-					</figure>
-					<div class="posts__inner">
-						<div class="posts__cat">
-							<span class="label posts__cat-label">The Team</span>
-						</div>
-						<h3 class="posts__title">Alchemists women team tryouts will start in January</h3>
-						<time datetime="2017-08-23" class="posts__date">August 23rd, 2018</time>
-						<ul class="post__meta meta">
-							<li class="meta__item meta__item--views">2369</li>
-							<li class="meta__item meta__item--likes"><i class="meta-like icon-heart"></i> 530</li>
-							<li class="meta__item meta__item--comments">18</li>
-						</ul>
-					</div>
-				</a>
-			</div>
-			<div class="posts__item posts__item--category-1">
-				<a href="#" class="posts__link-wrapper">
-					<figure class="posts__thumb">
-						<img src="assets/images/samples/featured-carousel-1.jpg" alt="">
-					</figure>
-					<div class="posts__inner">
-						<div class="posts__cat">
-							<span class="label posts__cat-label">The Team</span>
-						</div>
-						<h3 class="posts__title">All the players are taking a team trip this summer</h3>
-						<time datetime="2017-08-23" class="posts__date">August 23rd, 2018</time>
-						<ul class="post__meta meta">
-							<li class="meta__item meta__item--views">2369</li>
-							<li class="meta__item meta__item--likes"><i class="meta-like icon-heart"></i> 530</li>
-							<li class="meta__item meta__item--comments">18</li>
-						</ul>
-					</div>
-				</a>
-			</div>
-		
-		</div>
+<!-- Header Featured News -->
+<div class="posts posts--carousel-featured featured-carousel">
 
-		
-		<!-- Content
-		================================================== -->
-		<div class="site-content">
-			<div class="container">
-		
-				<div class="row">
-					<!-- Content -->
-					<div class="content col-md-8">
-		
-		
-		
+	<div class="posts__item posts__item--category-1">
+		<a href="#" class="posts__link-wrapper">
+			<figure class="posts__thumb">
+				<img src="assets/images/samples/featured-carousel-2.jpg" alt="">
+			</figure>
+			<div class="posts__inner">
+				<div class="posts__cat">
+					<span class="label posts__cat-label">The Team</span>
+				</div>
+				<h3 class="posts__title">Featured STory 1</h3>
+				<time datetime="2019-04-07" class="posts__date">April 7, 2019</time>
+				<ul class="post__meta meta">
+					<li class="meta__item meta__item--views">2369</li>
+					<li class="meta__item meta__item--likes"><i class="meta-like icon-heart"></i> 530</li>
+					<li class="meta__item meta__item--comments">18</li>
+				</ul>
+			</div>
+		</a>
+	</div>
+	<div class="posts__item posts__item--category-1">
+		<a href="#" class="posts__link-wrapper">
+			<figure class="posts__thumb">
+				<img src="assets/images/samples/featured-carousel-3.jpg" alt="">
+			</figure>
+			<div class="posts__inner">
+				<div class="posts__cat">
+					<span class="label posts__cat-label">The Team</span>
+				</div>
+				<h3 class="posts__title">Checkout the new ride of our best player of the season</h3>
+				<time datetime="2017-08-23" class="posts__date">August 23rd, 2018</time>
+				<ul class="post__meta meta">
+					<li class="meta__item meta__item--views">2369</li>
+					<li class="meta__item meta__item--likes"><i class="meta-like icon-heart"></i> 530</li>
+					<li class="meta__item meta__item--comments">18</li>
+				</ul>
+			</div>
+		</a>
+	</div>
+	<div class="posts__item posts__item--category-1">
+		<a href="#" class="posts__link-wrapper">
+			<figure class="posts__thumb">
+				<img src="assets/images/samples/featured-carousel-1.jpg" alt="">
+			</figure>
+			<div class="posts__inner">
+				<div class="posts__cat">
+					<span class="label posts__cat-label">The Team</span>
+				</div>
+				<h3 class="posts__title">All the players are taking a team trip this summer</h3>
+				<time datetime="2017-08-23" class="posts__date">August 23rd, 2018</time>
+				<ul class="post__meta meta">
+					<li class="meta__item meta__item--views">2369</li>
+					<li class="meta__item meta__item--likes"><i class="meta-like icon-heart"></i> 530</li>
+					<li class="meta__item meta__item--comments">18</li>
+				</ul>
+			</div>
+		</a>
+	</div>
+	<div class="posts__item posts__item--category-1">
+		<a href="#" class="posts__link-wrapper">
+			<figure class="posts__thumb">
+				<img src="assets/images/samples/featured-carousel-2.jpg" alt="">
+			</figure>
+			<div class="posts__inner">
+				<div class="posts__cat">
+					<span class="label posts__cat-label">The Team</span>
+				</div>
+				<h3 class="posts__title">Alchemists women team tryouts will start in January</h3>
+				<time datetime="2017-08-23" class="posts__date">August 23rd, 2018</time>
+				<ul class="post__meta meta">
+					<li class="meta__item meta__item--views">2369</li>
+					<li class="meta__item meta__item--likes"><i class="meta-like icon-heart"></i> 530</li>
+					<li class="meta__item meta__item--comments">18</li>
+				</ul>
+			</div>
+		</a>
+	</div>
+	<div class="posts__item posts__item--category-1">
+		<a href="#" class="posts__link-wrapper">
+			<figure class="posts__thumb">
+				<img src="assets/images/samples/featured-carousel-1.jpg" alt="">
+			</figure>
+			<div class="posts__inner">
+				<div class="posts__cat">
+					<span class="label posts__cat-label">The Team</span>
+				</div>
+				<h3 class="posts__title">All the players are taking a team trip this summer</h3>
+				<time datetime="2017-08-23" class="posts__date">August 23rd, 2018</time>
+				<ul class="post__meta meta">
+					<li class="meta__item meta__item--views">2369</li>
+					<li class="meta__item meta__item--likes"><i class="meta-like icon-heart"></i> 530</li>
+					<li class="meta__item meta__item--comments">18</li>
+				</ul>
+			</div>
+		</a>
+	</div>
 
+</div>
+<!-- Header Featured News / End -->
 
-		
-		
-						<!-- Widget: Social Buttons -->
-						<aside class="widget widget--sidebar widget-social">
-							<a href="#" class="btn-social-counter btn-social-counter--fb" target="_blank">
-								<div class="btn-social-counter__icon">
-									<i class="fa fa-facebook"></i>
-								</div>
-								<h6 class="btn-social-counter__title">Like Our Facebook Page</h6>
-								<span class="btn-social-counter__count"><span class="btn-social-counter__count-num"></span> Likes</span>
-								<span class="btn-social-counter__add-icon"></span>
-							</a>
-							<a href="#" class="btn-social-counter btn-social-counter--twitter" target="_blank">
-								<div class="btn-social-counter__icon">
-									<i class="fa fa-twitter"></i>
-								</div>
-								<h6 class="btn-social-counter__title">Follow Us on Twitter</h6>
-								<span class="btn-social-counter__count"><span class="btn-social-counter__count-num"></span> Followers</span>
-								<span class="btn-social-counter__add-icon"></span>
-							</a>
-							<a href="#" class="btn-social-counter btn-social-counter--instagram" target="_blank">
-								<div class="btn-social-counter__icon">
-									<i class="fa fa-instagram"></i>
-								</div>
-								<h6 class="btn-social-counter__title">Follow Us on Instagram</h6>
-								<span class="btn-social-counter__count"><span class="btn-social-counter__count-num"></span> Followers</span>
-								<span class="btn-social-counter__add-icon"></span>
-							</a>
-						</aside>
-						<!-- Widget: Social Buttons / End -->
-		
-						
-								<!-- Scoreboard -->
-								<div class="widget-game-result__section">
-									<div class="widget-game-result__table-stats">
-										<div class="table-responsive">
-											<table class="table table__cell-center table-thead-color">
-												<thead>
-													<tr>
-														<th>Scoreboard</th>
-													
-														<th>T</th>
-													</tr>
-												</thead>
-												<tbody>
-													<tr>
-														<th>U17 Girls  - Deep South</th>
-														 
-														<td>107</td>
-													</tr>
-													<tr>
-														<th>U17 Xment</th>
-														 
-														<td>102</td>
-													</tr>
-												</tbody>
-											</table>
-										</div>
-									</div>
-								</div>
-								<!-- Scoreboard / End -->
-						
-		
-						<!-- Widget: Newsletter -->
-						<aside class="widget widget--sidebar card widget-newsletter">
-							<div class="widget__title card__header">
-								<h4>Our Newsletter</h4>
+<!-- Content -->
+<div class="site-content">
+	<div class="container">
+		<div class="row">
+			<!-- Content -->
+
+			<!-- Scoreboard -->
+			<div class="widget-game-result__section col-md-8">
+				<div class="widget-game-result__table-stats">
+					<div class="table-responsive">
+						<table class="table table__cell-center table-thead-color">
+							<thead>
+								<tr>
+									<th>Scoreboard</th>
+
+									<th>T</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<th>U17 Girls - Deep South</th>
+
+									<td>107</td>
+								</tr>
+								<tr>
+									<th>U17 Xment</th>
+
+									<td>102</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+				<!-- Scoreboard / End -->
+
+				<!-- Widget: Newsletter -->
+				<aside class="widget widget--sidebar card widget-newsletter">
+					<div class="widget__title card__header">
+						<h4>Our Newsletter</h4>
+					</div>
+					<div class="widget__content card__content">
+						<h5 class="widget-newsletter__subtitle">Subscribe Now!</h5>
+						<div class="widget-newsletter__desc">
+							<p>Receive the latest news from the team: game reminders, new adquisitions and professional match results.</p>
+						</div>
+						<form action="#" id="newsletter" class="inline-form">
+							<div class="input-group">
+								<input type="email" class="form-control" placeholder="Your email address...">
+								<span class="input-group-append">
+									<button class="btn btn-lg btn-default" type="button">Send</button>
+								</span>
 							</div>
-							<div class="widget__content card__content">
-								<h5 class="widget-newsletter__subtitle">Subscribe Now!</h5>
-								<div class="widget-newsletter__desc">
-									<p>Receive the latest news from the team: game reminders, new adquisitions and professional match results.</p>
-								</div>
-								<form action="#" id="newsletter" class="inline-form">
-									<div class="input-group">
-										<input type="email" class="form-control" placeholder="Your email address...">
-										<span class="input-group-append">
-											<button class="btn btn-lg btn-default" type="button">Send</button>
-										</span>
-									</div>
-								</form>
-							</div>
-						</aside>
-						<!-- Widget: Newsletter / End -->
-		
+						</form>
+					</div>
+				</aside>
 
-    
-    <?php require_once "footer.php";
+			</div>
+			<!-- Widget: Social Buttons -->
+			<aside class="widget widget--sidebar widget-social col-md-4">
+				<a href="#" class="btn-social-counter btn-social-counter--fb" target="_blank">
+					<div class="btn-social-counter__icon">
+						<i class="fa fa-facebook"></i>
+					</div>
+					<h6 class="btn-social-counter__title">Like Our Facebook Page</h6>
+					<span class="btn-social-counter__count"><span class="btn-social-counter__count-num"></span> Likes</span>
+					<span class="btn-social-counter__add-icon"></span>
+				</a>
+				<a href="#" class="btn-social-counter btn-social-counter--twitter" target="_blank">
+					<div class="btn-social-counter__icon">
+						<i class="fa fa-twitter"></i>
+					</div>
+					<h6 class="btn-social-counter__title">Follow Us on Twitter</h6>
+					<span class="btn-social-counter__count"><span class="btn-social-counter__count-num"></span> Followers</span>
+					<span class="btn-social-counter__add-icon"></span>
+				</a>
+				<a href="#" class="btn-social-counter btn-social-counter--instagram" target="_blank">
+					<div class="btn-social-counter__icon">
+						<i class="fa fa-instagram"></i>
+					</div>
+					<h6 class="btn-social-counter__title">Follow Us on Instagram</h6>
+					<span class="btn-social-counter__count"><span class="btn-social-counter__count-num"></span> Followers</span>
+					<span class="btn-social-counter__add-icon"></span>
+				</a>
+			</aside>
+			<!-- Widget: Social Buttons / End -->
+		</div>
+		<?php require_once "footer.php";
